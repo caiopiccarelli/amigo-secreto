dcd2a65f9158a45d78dc547eb41b628bf14a7ec9
caio <caio@ipa-aip.org>
2025-06-04 15:37:11 -0300

    amigo secreto

---
+++ js/app.js
@@ -0,0 +1,56 @@
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Informe um nome antes de clicar em Adicionar');
+        return;
+    }
+    lista = document.getElementById('lista-amigos');
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já cadastrado');
+        return;
+    }
+    amigos.push(amigo.value);
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+    document.getElementById('nome-amigo').focus();
+    //console.log(listaAmigos);
+}
+
+function sortear() {
+    if (amigos.length < 4) {
+        alert('Informe pelo menos o nome de 4 pessoas para realizar o sorteio');
+        return;
+    }
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' >> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' >> ' + amigos[i + 1] + '<br>';
+        }
+    }
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+    document.getElementById('nome-amigo').focus();
+}
+
